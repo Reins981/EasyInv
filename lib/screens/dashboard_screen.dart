b6059af2e819281de9e3744c87eaf9aa73ecf1a2
reko8680 <reins981@gmail.com>
2024-04-07 20:37:11 +0200

    Add Bar charts part3

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -14,16 +14,34 @@ class DashboardScreen extends StatefulWidget {
   _DashboardScreenState createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class _DashboardScreenState extends State<DashboardScreen> with SingleTickerProviderStateMixin{
   final FirestoreService firestoreService = FirestoreService();
   final ScrollController _scrollController = ScrollController();
+  late final AnimationController _animationController;
+  late Animation<double> _animation;
   Helper helper = Helper();
 
   @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut, // Adjust curve as needed
+    );
+    _animationController.forward(); // Start the animation
+  }
+
+  @override
   void dispose() {
-    _scrollController.dispose();
+    _scrollController.dispose(); // Dispose the animation controller
+    _animationController.dispose(); // Dispose the animation controller
     super.dispose();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -72,11 +90,31 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _buildTotalProfitWidget(totalProfit),
-                    _buildStatCard('Total Items', totalItems),
+                    _buildTotalProfitWidget(context, totalProfit),
                     const SizedBox(height: 20),
-                    _buildStatCard('Total Categories', totalCategories),
-                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _buildStatCard('Total Items', totalItems),
+                              const SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _buildStatCard('Total Categories', totalCategories),
+                              const SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                     lowStockItems.isNotEmpty ? _buildLowStockItemsCard(lowStockItems) : Container(),
                     const SizedBox(height: 20),
                     _buildInventoryChart(snapshot.data!, chartHeight),
@@ -136,58 +174,116 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
-  Widget _buildTotalProfitWidget(double totalProfit) {
+  Widget _buildTotalProfitWidget(BuildContext context, double totalProfit) {
     return Container(
-      margin: EdgeInsets.only(bottom: 16.0), // Adjust margin as needed
-      padding: EdgeInsets.all(8.0), // Add padding to give space around the text
+      margin: EdgeInsets.only(bottom: 16.0),
+      padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: AppColors.rosa, // Background color of the rectangle
-        borderRadius: BorderRadius.circular(12.0), // Rounded corners
+        gradient: const LinearGradient(
+          colors: [Colors.purpleAccent, Colors.pink],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 6.0,
+            offset: Offset(0, 2),
+          ),
+        ],
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            '$totalProfit', // Total profit formatted as currency with $ sign
-            style: const TextStyle(
-              fontSize: 30.0, // Customize font size
-              fontWeight: FontWeight.bold, // Add bold font weight
-              color: Colors.white, // Text color
-              fontFamily: 'Roboto', // Customize font family if needed
-              letterSpacing: 1.0, // Add letter spacing
-            ),
+          Row(
+            children: [
+              ScaleTransition(
+                scale: _animation, // Assuming _animation is defined in your State class
+                child: const Icon(
+                  Icons.attach_money,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '\$$totalProfit',
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          const Icon(
-            Icons.attach_money, // Dollar sign icon
-            color: Colors.white, // Icon color
-            size: 30.0, // Customize size
+          Column(
+            children: [
+              _buildInventoryButton(context),
+            ],
           ),
         ],
       ),
     );
   }
 
+  Widget _buildInventoryButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Handle inventory button tap
+      },
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.purpleAccent, backgroundColor: Colors.white, shape: CircleBorder(),
+        padding: const EdgeInsets.all(16.0),
+        elevation: 4,
+      ),
+      child: const Icon(
+        Icons.inventory,
+        color: Colors.purpleAccent,
+        size: 30.0,
+      ),
+    );
+  }
+
   Widget _buildStatCard(String title, int value) {
+    Icon resultIcon = title == 'Total Items'
+        ? const Icon(Icons.shopping_cart, color: Colors.white, size: 60.0)
+        : const Icon(Icons.category, color: Colors.white, size: 60.0);
+
     return Card(
       elevation: 4.0,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       color: AppColors.rosa,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: AppColors.white),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return SizedBox(
+            width: constraints.maxWidth,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  resultIcon,
+                  const SizedBox(height: 8.0),
+                  Text(
+                    value.toString(),
+                    style: const TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 8.0),
-            Text(
-              value.toString(),
-              style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: AppColors.white),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -410,7 +506,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
   }
 
   Widget _buildInventoryChart(List<Item> items, double chartHeight) {
-    items.sort((a, b) => a.vendor.compareTo(b.vendor));
+    items.sort((a, b) {
+      // First, compare by vendor
+      int vendorComparison = a.vendor.compareTo(b.vendor);
+      if (vendorComparison != 0) {
+        return vendorComparison;
+      }
+
+      // If vendors are equal, compare by category
+      return a.category.compareTo(b.category);
+    });
+
     final data = [
       charts.Series<Item, String>(
         id: 'Inventory',
@@ -437,7 +543,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
               vertical: false, // Set vertical to false for horizontal orientation
               barRendererDecorator: charts.BarLabelDecorator<String>(),
               domainAxis: const charts.OrdinalAxisSpec(
-                  renderSpec: charts.NoneRenderSpec()),
+                renderSpec: charts.NoneRenderSpec(),
+              ),
+              defaultRenderer: charts.BarRendererConfig<String>(
+                cornerStrategy: const charts.ConstCornerStrategy(12),
+                barRendererDecorator: charts.BarLabelDecorator<String>(),
+                // Adjust the radius for rounded corners
+              ),
             ),
           ),
         ),
